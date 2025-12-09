40dd70f56160c302dfdb03e0c11fb5356c6e9628
Jay Aldrin O. Rabi <jayaldrinorabi015@gmail.com>
2025-12-09 13:13:11 +0800

    editing paring BOI!

--- ASSETS/MEMBER/index.php
+++ ASSETS/MEMBER/index.php
@@ -5,14 +5,41 @@
 
 <?php include "../../header.php"?>
 
-<section class="">
+<section class="members-section">
   <div class="member-container">
-    <div class="row">
+    <div class="member-add-box row">
       <div class="col-md-6">
         <h2>Library Members</h2>
       </div>
       <div class="col-md-6">
-        <button>+ Add Member</button>
+        <button class="btn btn-primary">+ Add Member</button>
+      </div>
+    </div>
+
+    <div class="row member-list">
+      <div class="row">
+        <div class="container mt-3">
+          <div class="card p-3 d-flex flex-row align-items-center justify-content-between shadow-sm contact-item">
+
+            <!-- LEFT SIDE -->
+            <div class="d-flex align-items-center ">
+
+              <div class="text-muted small">
+                <h6 class="mb-1 fw-bold">Alice Johnson</h6>
+                <i class="bi bi-envelope me-1"></i> alice@email.com
+                <span class="ms-3"><i class="bi bi-telephone me-1"></i> (555) 123-4567</span>
+              </div>
+
+              <div class="">
+                <button class="btn btn-sm btn-outline-secondary ms-3">Edit</button>
+                <button class="btn btn-sm btn-outline-danger ms-2">Delete</button>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
